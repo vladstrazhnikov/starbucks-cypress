d4369486f7e0b614280d0b93f6a6b480b3c64d50
vladstrazhnikov <vladislav.strazhnikov@gmail.com>
2022-05-25 15:02:23 +0400

    Test Case 2

--- cypress/pages/MainPage.js
+++ cypress/pages/MainPage.js
@@ -1,3 +1,4 @@
 export class MainPage {
     joinButton = '//a[3][contains(@href, "/account/create")]'
+    signInButton = '//a[2][contains(@href, "/account/signin")]'
 }
---
+++ cypress/pages/SignInPage.js
@@ -0,0 +1,6 @@
+export class SignInPage {
+    usernameInput = '#username'
+    passwordInput = '#password'
+    signInButton = '[data-e2e=signInButton]'
+    signInUnsuccessfulNotification = '.sb-expander .sb-heading'
+}
