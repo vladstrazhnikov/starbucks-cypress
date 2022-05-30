9532dd8e957ba220decb8a3ad82eb183377cc314
vladstrazhnikov <vladislav.strazhnikov@gmail.com>
2022-05-31 01:43:33 +0400

    Test Case 3

--- cypress/pages/MainPage.js
+++ cypress/pages/MainPage.js
@@ -1,4 +1,10 @@
 export class MainPage {
     joinButton = '//a[3][contains(@href, "/account/create")]'
     signInButton = '//a[2][contains(@href, "/account/signin")]'
+    findStoreButton = '//a[1][contains(@href, "/store-locator")]'
+
+    clickButton (locator, url) {
+        cy.xpath(locator).click()
+        cy.url().should('include', url)
+    }
 }
