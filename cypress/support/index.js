8e5f46a76ebdefe27d7e93bed3759a54c262ced1
vladstrazhnikov <vladislav.strazhnikov@gmail.com>
2022-06-10 14:30:56 +0400

    mochawesome reporter

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-mochawesome-reporter/register';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
@@ -25,3 +26,4 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // failing the test
     return false
   })
+  
